9bde5d8f7e974b7705b3ee873114183e7fa2e0e3
Sergio <sergio.chaparro@weibook.co>
2025-11-21 22:40:43 -0500

    Fix calculation of total drawn parts in Game class to use wrongLetters size instead of wrong attempts count.

--- src/main/java/com/ean/poo/model/Game.java
+++ src/main/java/com/ean/poo/model/Game.java
@@ -228,7 +228,7 @@ public class Game {
 
     public int getTotalDrawnParts() {
         int hintsUsed = TOTAL_HINTS - availableHints;
-        return getWrongAttemptsCount() + hintsUsed;
+        return wrongLetters.size() + hintsUsed;
     }
 
     public void setRemainingAttempts(int attempts) {
